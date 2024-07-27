8a57a98e52d1c8b9cd256e7afc031525074d6879
allenlin <ht016746@gmail.com>
2024-07-27 15:49:45 +0700

    refactor: change bcrypt to generic encryption methods

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { BcryptService } from './bcrypt.service';
+import { EncryptionService } from './encryption.service';
 
 @Module({
   imports: [UsersModule],
   controllers: [AuthController],
   providers: [AuthService, BcryptService],
+  providers: [
+    AuthService,
+    EncryptionService,
+  ],
 })
 export class AuthModule {}
--- src/auth/encryption.service.ts
+++ src/auth/encryption.service.ts
@@ -2,8 +2,12 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class BcryptService {
+export class EncryptionService {
   hash(data: string | Buffer, saltOrRounds: string | number): Promise<string> {
     return bcrypt.hash(data, saltOrRounds);
+  }
+
+  compare(password: string, storedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, storedPassword);
   }
 }
