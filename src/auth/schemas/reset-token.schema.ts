841b4203e1fd0037804a9e8fa56b182c5c222d11
allenlin <ht016746@gmail.com>
2024-08-01 00:36:32 +0700

    Squashed commit of the following:
    commit 8237e3fc82c5f1f2b9a2ba0f7e512ad7b0f93b8e
    Author: allenlin <ht016746@gmail.com>
    Date:   Thu Aug 1 00:36:16 2024 +0700
    
        chore: cascade reference delete in app layer

--- src/auth/schemas/reset-token.schema.ts
+++ src/auth/schemas/reset-token.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from '../../users/schemas/user.schema';
 
 export type ResetTokenDocument = HydratedDocument<ResetToken>;
 
@@ -8,7 +9,7 @@ export class ResetToken {
   @Prop({ required: true })
   token: string;
 
-  @Prop({ required: true, type: mongoose.Types.ObjectId })
+  @Prop({ required: true, type: mongoose.Types.ObjectId, ref: User.name })
   userId: string;
 
   @Prop({ required: true })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,33 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersRepository } from './users.repository';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
+import { ResetToken } from 'src/auth/schemas/reset-token.schema';
+import { RefreshToken } from 'src/auth/schemas/refresh-token.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+          schema.pre('deleteOne', async function () {
+            const { _id: userId } = this.getQuery();
+            const ResetTokenModel = this.model.db.model<ResetToken>(
+              ResetToken.name,
+            );
+            const RefreshTokenModel = this.model.db.model<RefreshToken>(
+              RefreshToken.name,
+            );
+
+            await ResetTokenModel.deleteMany({ userId });
+            await RefreshTokenModel.deleteMany({ userId });
+          });
+
+          return schema;
+        },
+      },
+    ]),
   ],
   providers: [UsersService, UsersRepository],
   exports: [UsersService],
